64882cad1b561cb4de27b00b926971a3e52c462d
yogeshpan1 <pantyogeshh@gmail.com>
2026-07-17 17:16:38 +0545

    Making some code changes

--- appointments/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Doctor, DoctorTimeSlot, Appointment
-
-admin.site.register(Doctor)
-admin.site.register(DoctorTimeSlot)
-admin.site.register(Appointment)

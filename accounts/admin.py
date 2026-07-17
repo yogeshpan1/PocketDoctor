938efc3331776794ef7c7d5f0a6ff643ab47be6f
yogeshpan1 <pantyogeshh@gmail.com>
2026-07-17 16:33:34 +0545

    adding the user

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    # Add 'role' to the fields shown in the admin list and edit form
+    list_display = ('username', 'email', 'role', 'is_staff', 'is_active')
+    fieldsets = UserAdmin.fieldsets + (
+        ('Role Info', {'fields': ('role', 'phone_number', 'email_verified')}),
+    )
+
+
+admin.site.register(User, CustomUserAdmin)

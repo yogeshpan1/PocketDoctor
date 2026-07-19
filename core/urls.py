3abb6ca1a138d23ffb7cb34fd81f7dfd5b9507f9
yogeshpan1 <pantyogeshh@gmail.com>
2026-07-19 23:51:18 +0545

    Adding the ECG

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import TemplateView
+from core.views import home_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('triage/', include('triage.urls')),
     path('appointments/', include('appointments.urls')),
+    path('', home_view, name='home'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from appointments.models import Doctor
+import random
 
 
 def home_view(request):
-    doctors = Doctor.objects.filter(is_active=True)[:4]
-    return render(request, 'home.html', {'doctors': doctors})
+    all_doctors = list(Doctor.objects.filter(is_active=True))
+    featured_doctor = random.choice(all_doctors) if all_doctors else None
+    doctors_list = all_doctors[:4]
+    return render(request, 'home.html', {
+        'doctors': doctors_list,
+        'featured_doctor': featured_doctor,
+    })

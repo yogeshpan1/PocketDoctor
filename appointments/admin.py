075d662020e737d9edb2b9368e8c95394237d08d
yogeshpan1 <pantyogeshh@gmail.com>
2026-07-20 13:14:17 +0545

    Add Message and Prescription models, link Doctor to User account

--- appointments/admin.py
+++ appointments/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Doctor, DoctorTimeSlot, Appointment
+from .models import Doctor, DoctorTimeSlot, Appointment, Message, Prescription
 
 admin.site.register(Doctor)
 admin.site.register(DoctorTimeSlot)
 admin.site.register(Appointment)
+admin.site.register(Message)
+admin.site.register(Prescription)
